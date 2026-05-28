894e6317e089692c165dc5c4b01b60459152f441
Ryan Gabriel <ryangabri306@gmail.com>
2026-05-27 21:32:22 -0300

    Criando classes iniciais

---
+++ src/Reserva.java
@@ -0,0 +1,28 @@
+public class Reserva {
+    private Hospede hospede;
+    private Quarto quarto;
+    private boolean ativa;
+
+    public Reserva(Hospede hospede, Quarto quarto) {
+        this.hospede = hospede;
+        this.quarto = quarto;
+        this.ativa = true;
+    }
+
+    public void finalizarReserva(){
+        ativa = false;
+    }
+
+    public boolean isAtiva(){
+        return ativa;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+}
